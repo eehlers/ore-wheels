ee1e6cb353017896ad702812f026eec4e28333d9
Eric Ehlers <eric.ehlers@reposit.be>
2023-02-02 14:02:08 +0000

    WIP

---
+++ before_all_linux.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+set -e
+
+# Setup Boost
+#curl -O -L https://boostorg.jfrog.io/artifactory/main/release/1.81.0/source/boost_1_81_0.tar.gz
+#tar xfz boost_*.tar.gz
+#cd boost_*/
+#mv boost /usr/local/include/
+#cd ..
+
+#apt-get install libboost-all-dev
+
+# Unpack ORE & ORE-SWIG
+tar xfz ore.00.tgz
+tar xfz oreswig.01.tgz
+
+# Build ORE
+cd ore
+mkdir build
+cd build
+cmake -DCMAKE_POSITION_INDEPENDENT_CODE=ON -DBUILD_DOC=OFF -DBoost_NO_WARN_NEW_VERSIONS=1 -DBoost_NO_SYSTEM_PATHS=1 ..
+cmake --build .
+
