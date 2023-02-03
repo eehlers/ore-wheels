35ad7fbaaa9dbfd2332046ceee269a9bdca593ef
Eric Ehlers <eric.ehlers@reposit.be>
2023-02-03 14:57:54 +0000

    WIP

--- before_all_linux.sh
+++ before_all_linux.sh
@@ -15,12 +15,13 @@ BOOST_ROOT=/home/runner/work/ore-wheels/ore-wheels/boost/boost
 # Unpack ORE & ORE-SWIG
 tar xfz ore.00.tgz
 tar xfz oreswig.01.tgz
+cp setup.py oreswig/OREAnalytics-SWIG/Python
 
 # Build ORE
-cd ore
-mkdir build
-cd build
-#cmake -DCMAKE_POSITION_INDEPENDENT_CODE=ON -DBUILD_DOC=OFF -DBoost_NO_WARN_NEW_VERSIONS=1 -DBoost_NO_SYSTEM_PATHS=1 -DBOOST_ROOT=/home/runner/work/ore-wheels/ore-wheels/boost/boost ..
-cmake -DCMAKE_POSITION_INDEPENDENT_CODE=ON -DBUILD_DOC=OFF -DBoost_NO_WARN_NEW_VERSIONS=1 -DBoost_NO_SYSTEM_PATHS=1 ..
-cmake --build .
+#cd ore
+#mkdir build
+#cd build
+##cmake -DCMAKE_POSITION_INDEPENDENT_CODE=ON -DBUILD_DOC=OFF -DBoost_NO_WARN_NEW_VERSIONS=1 -DBoost_NO_SYSTEM_PATHS=1 -DBOOST_ROOT=/home/runner/work/ore-wheels/ore-wheels/boost/boost ..
+#cmake -DCMAKE_POSITION_INDEPENDENT_CODE=ON -DBUILD_DOC=OFF -DBoost_NO_WARN_NEW_VERSIONS=1 -DBoost_NO_SYSTEM_PATHS=1 ..
+#cmake --build .
 
--- test1.py
+++ test1.py
@@ -1,5 +1,7 @@
 import os
 
+ORE=os.environ['ORE']
+print(f'ORE={ORE}')
 ql_compile_args = os.popen('./oreanalytics-config --cflags').read()[:-1].split()
 print('ql_compile_args={}'.format(ql_compile_args))
 
