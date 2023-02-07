ee1c2555679b4d15ea603b80b1e2d1d9782ecc69
Eric Ehlers <eric.ehlers@reposit.be>
2023-02-07 22:00:23 +0100

    WIP

--- before_all_linux.22.sh
+++ before_all_linux.22.sh
@@ -28,6 +28,7 @@ cd ore
 mkdir build
 cd build
 cmake -DCMAKE_POSITION_INDEPENDENT_CODE=ON -DBUILD_DOC=OFF ..
+cmake --build .
 cmake --install .
 
 echo "@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@"
