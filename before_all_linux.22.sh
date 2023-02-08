f9472698c7bd6f4b163037b3ea4956fc26349d51
Eric Ehlers <eric.ehlers@reposit.be>
2023-02-08 17:06:25 +0100

    WIP

--- before_all_linux.22.sh
+++ before_all_linux.22.sh
@@ -11,16 +11,11 @@ echo "@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@"
 curl -O -L https://boostorg.jfrog.io/artifactory/main/release/1.80.0/source/boost_1_80_0.tar.gz
 tar xfz boost_1_80_0.tar.gz
 cd boost_1_80_0
-./bootstrap.sh --with-libraries=date_time,filesystem,regex,serialization,system,thread,timer,unit_test_framework
+# FIXME the below fails because boost_serialization can't find boost_unit_test_framework?
+#./bootstrap.sh --with-libraries=date_time,filesystem,regex,serialization,system,thread,timer
+./bootstrap.sh
 ./b2 install
 cd ..
-#ls -l /home/runner/work/ore-wheels/ore-wheels/boost/lib
-
-## Unpack ORE & ORE-SWIG
-#tar xfz ore.00.tgz
-#tar xfz oreswig.01.tgz
-#cp setup.22.py oreswig/OREAnalytics-SWIG/Python/setup.py
-#cp oreanalytics-config.22 oreswig/OREAnalytics-SWIG/oreanalytics-config
 
 # Build ORE
 cd ore
@@ -29,10 +24,4 @@ cd build
 cmake -DCMAKE_POSITION_INDEPENDENT_CODE=ON -DBUILD_DOC=OFF ..
 cmake --build .
 cmake --install .
-
-echo "@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@"
-echo "pwd"
-pwd
-echo "@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@"
-
 
