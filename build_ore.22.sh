050db02b559867adbce5bc572d2a4a4e406ab35c
Eric Ehlers <eric.ehlers@reposit.be>
2023-02-21 17:16:27 +0100

    WIP

--- build_ore.22.sh
+++ build_ore.22.sh
@@ -8,7 +8,8 @@ pwd
 echo "@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@"
 
 # Unpack ORE & ORE-SWIG
-tar xfz ore.00.tgz
+#tar xfz ore.00.tgz
+tar xfz ore.01.tgz
 tar xfz oreswig.01.tgz
 cp setup.22.py oreswig/OREAnalytics-SWIG/Python/setup.py
 cp oreanalytics-config.22 oreswig/OREAnalytics-SWIG/oreanalytics-config
