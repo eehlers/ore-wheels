95aa3bf27ab5f77dad6fe98f6a0b42b4b44a91b0
Eric Ehlers <eric.ehlers@reposit.be>
2023-03-02 16:32:33 +0000

    WIP

--- build_ore.22.sh
+++ build_ore.22.sh
@@ -11,12 +11,9 @@ echo "@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@"
 #tar xfz ore.00.tgz
 #tar xfz ore.01.tgz
 tar xfz ore.02.tgz
+# Patch a bug in our copy of quantlib
+cp exchangerate.hpp ore/QuantLib/ql
 tar xfz oreswig.01.tgz
 cp setup.22.py oreswig/OREAnalytics-SWIG/Python/setup.py
 cp oreanalytics-config.22 oreswig/OREAnalytics-SWIG/oreanalytics-config
-# If you do
-# -DCMAKE_BUILD_TYPE=Release
-# Or if you run the musllinux build (different optimization),
-# you need the hack below.  FIXME variable not properly initialized
-cp fxforward.cpp ore/QuantExt/qle/instruments
 
