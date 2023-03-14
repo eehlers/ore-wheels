6e1ca7a51ff089f06474c24d39265230e28f8caf
Eric Ehlers <eric.ehlers@acadia.inc>
2023-03-14 18:43:31 +0000

    WIP

--- build_ore.22.sh
+++ build_ore.22.sh
@@ -12,9 +12,10 @@ echo "@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@"
 #tar xfz ore.01.tgz
 #tar xfz ore.02.tgz
 tar xfz ore.03.tgz
-tar xfz oreswig.02.tgz
 # Patch a bug in our copy of quantlib
 cp exchangerate.hpp ore/QuantLib/ql
+cp cpivolatilitystructure.hpp QuantExt/qle/termstructures/inflation/cpivolatilitystructure.hpp
+tar xfz oreswig.02.tgz
 cp setup.22.py oreswig/OREAnalytics-SWIG/Python/setup.py
 cp oreanalytics-config.22 oreswig/OREAnalytics-SWIG/oreanalytics-config
 
