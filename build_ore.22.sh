b8fc630f37d41e9bb8df312907c2c7b4eafe8d55
Eric Ehlers <eric.ehlers@reposit.be>
2023-03-22 20:39:46 +0100

    WIP

--- build_ore.22.sh
+++ build_ore.22.sh
@@ -11,10 +11,10 @@ echo "@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@"
 #tar xfz ore.00.tgz
 #tar xfz ore.01.tgz
 #tar xfz ore.02.tgz
-tar xfz ore.03.tgz
+tar xfz ore.tgz
 # Patch a bug in our copy of quantlib
-cp exchangerate.hpp ore/QuantLib/ql
-tar xfz oreswig.02.tgz
+#cp exchangerate.hpp ore/QuantLib/ql
+tar xfz oreswig.tgz
 cp setup.22.py oreswig/OREAnalytics-SWIG/Python/setup.py
-cp oreanalytics-config.22 oreswig/OREAnalytics-SWIG/oreanalytics-config
+#cp oreanalytics-config.22 oreswig/OREAnalytics-SWIG/oreanalytics-config
 
