18a6e716bb018bfd0090d60f4964f4070d2d4ceb
Eric Ehlers <eric.ehlers@acadia.inc>
2023-04-13 17:00:45 +0100

    WIP

--- before_all_linux.22.sh
+++ before_all_linux.22.sh
@@ -7,19 +7,29 @@ echo "pwd"
 pwd
 echo "@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@"
 
+CURRENT_DIR=$(pwd)
+
+curl -O -L https://www.zlib.net/zlib-1.2.13.tar.gz
+tar xzvf zlib-1.2.13.tar.gz
+cd zlib-1.2.13
+./configure
+make
+cd ..
+
 # Setup Boost
 curl -O -L https://boostorg.jfrog.io/artifactory/main/release/1.80.0/source/boost_1_80_0.tar.gz
 tar xfz boost_1_80_0.tar.gz
 cd boost_1_80_0
 ./bootstrap.sh --with-libraries=date_time,filesystem,iostreams,regex,serialization,system,thread,timer
-./b2 install
+./b2 install -sZLIB_SOURCE=$CURRENT_DIR/zlib-1.2.13
 cd ..
 
 # Build ORE
 cd ore
 mkdir build
 cd build
-cmake -DCMAKE_POSITION_INDEPENDENT_CODE=ON -DORE_BUILD_DOC=OFF -DORE_BUILD_EXAMPLES=OFF -DORE_BUILD_TESTS=OFF -DORE_BUILD_APP=OFF -DQL_BUILD_BENCHMARK=OFF -DQL_BUILD_EXAMPLES=OFF -DQL_BUILD_TEST_SUITE=OFF -DCMAKE_BUILD_TYPE=Release ..
+export ZLIB_ROOT=$CURRENT_DIR/zlib-1.2.13
+cmake -DCMAKE_POSITION_INDEPENDENT_CODE=ON -DORE_BUILD_DOC=OFF -DORE_BUILD_EXAMPLES=OFF -DORE_BUILD_TESTS=OFF -DORE_BUILD_APP=OFF -DQL_BUILD_BENCHMARK=OFF -DQL_BUILD_EXAMPLES=OFF -DQL_BUILD_TEST_SUITE=OFF -DCMAKE_BUILD_TYPE=Release -DORE_USE_ZLIB=ON ..
 cmake --build . -j4
 cmake --install .
 
